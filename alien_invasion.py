981e5b6d401102d7b18fd9e7a84625d0f578e562
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-07-12 18:06:52 -0400

    Created a Ship class and set up the ship image and position.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -17,6 +18,8 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
+        self.ship = Ship(self)
+
     def run_game(self):
         # Game loop
         while self.running:
@@ -26,7 +29,8 @@ class AlienInvasion:
                     pygame.quit()
                     sys.exit()
 
-            self.screen.blit(self.bg_image, (0, 0))
+            self.screen.blit(self.bg, (0, 0))
+            self.ship.draw()
             pygame.display.flip()
             self.clock.tick(self.settings.FPS)
 
