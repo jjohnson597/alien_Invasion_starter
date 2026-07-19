edc265f8a278a2b2f502a248ca246d755d3113eb
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-07-19 19:16:28 -0400

    Breakfix

--- alien_fleet.py
+++ alien_fleet.py
@@ -1,15 +1,19 @@
 import pygame
 from typing import TYPE_CHECKING
 
+from alien import Alien
+
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
-    from alien import Alien
-    
+
+
 class AlienFleet:
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: "AlienInvasion"):
         self.game = game
         self.settings = game.settings
+
         self.aliens = pygame.sprite.Group()
+
         self.alien_direction = self.settings.alien_direction
         self.alien_drop_speed = self.settings.alien_drop_speed
 
@@ -19,19 +23,23 @@ class AlienFleet:
         alien_width = self.settings.alien_width
         screen_width = self.settings.screen_width
 
-        fleet_width = self.calculate_fleet_size(alien_width, screen_width)
+        fleet_width = self.calculate_fleet_size(
+            alien_width,
+            screen_width
+        )
 
-        half_screen = self.settings.screen_width // 2
-        fleet_horizontal_spacing = (fleet_width * alien_width)
-        x_offset = (screen_width - fleet_horizontal_spacing) // 2
+        fleet_horizontal_spacing = fleet_width * alien_width
+        x_offset = (
+            screen_width - fleet_horizontal_spacing
+        ) // 2
 
         for col in range(fleet_width):
             current_x = x_offset + (col * alien_width)
-            self._create_alien(self, current_x, 10)
-
+            self._create_alien(current_x, 10)
 
     def calculate_fleet_size(self, alien_width, screen_width):
         fleet_width = screen_width // alien_width
+
         if fleet_width % 2 == 0:
             fleet_width -= 1
         else:
@@ -40,13 +48,14 @@ class AlienFleet:
         return fleet_width
 
     def _create_alien(self, current_x, current_y):
-        new_alien = Alien(self, current_x, current_y)
+        new_alien = Alien(
+            self,
+            current_x,
+            current_y
+        )
 
-        self.fleet.add(new_alien)
+        self.aliens.add(new_alien)
 
     def draw(self):
-        alien: "Alien"
-        for alien in self.fleet:
+        for alien in self.aliens:
             alien.draw_alien()
-
-        
