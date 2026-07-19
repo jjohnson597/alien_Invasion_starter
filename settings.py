339b7c973376456e2cd5261fb0806bcc3171ee39
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-07-19 17:53:01 -0400

    Create: Alien display on screen Please enter the commit message for your changes. Lines starting

--- settings.py
+++ settings.py
@@ -22,3 +22,10 @@ class Settings:
         self.bullet_color = (255, 0, 0)
         self.bullet_amount = 5
 
+        self.alien_file = pathlib.Path.cwd() / "Assets" / "images" / "enemy_4.png"
+        self.alien_width = 40
+        self.alien_height = 40
+        self.alien_speed = 5
+
+        
+
